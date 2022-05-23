13411cc2fced285c0857d03741cebe5e06b6a1ea
lsuinl <su96in43@naver.com>
2022-05-23 22:39:27 +0900

    2022.05.23

--- class0517.java
+++ class0517.java
@@ -1,56 +1,59 @@
-import java.util.Scanner;
+//프로그래머스 문제: 1단계.신규 아이디 추천
+//정규표현식 공부, 줄여보기 https://velog.io/@daehong770/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%97%B0%EC%8A%B5-Level-1-%EB%A1%9C%EB%98%90%EC%9D%98-%EC%B5%9C%EA%B3%A0-%EC%88%9C%EC%9C%84%EC%99%80-%EC%8B%A0%EA%B7%9C-%EC%95%84%EC%9D%B4%EB%94%94-%EC%9E%90%EB%B0%94-Java
 
 public class class0517 {
     public static void main(String[] args){  
-        String new_id;
-        Scanner sc =new Scanner(System.in);
-        new_id=sc.nextLine();
         
-        //솔루션 함수
-
-        //대소문자 치환
-        new_id.toLowerCase();
-
-        //문자 제외
-        for(int i=0; i<new_id.length();i++){
-            
+        class Solution {
+            public String solution(String new_id) {
+                //1단계
+                String answer =new_id.toLowerCase();
+                //2단계
+                for(int i=0; i<answer.length();i++){
+                    int a=answer.charAt(i);
+                    
+                    if( (96<a && 123>a) || (47<a && 58>a) ||a==95 || a==45 ||a==46){
+                    }
+                    else
+                    {
+                        answer=answer.replace(answer.charAt(i), ' ');
+                    }
+                    
+                }
+                answer=answer.replaceAll(" ","");
+                
+                //3단계
+                while(answer.contains("..")){
+                    answer=answer.replace("..", ".");
+                }
+                //4단계
+                while(answer.charAt(0)=='.' || answer.charAt(answer.length()-1)=='.'){
+                    if(answer.length()<=1){
+                        answer="";
+                        break;
+                    }
+                    if(answer.charAt(0)=='.')
+                        answer=answer.substring(1, answer.length());
+                    else
+                        answer=answer.substring(0, answer.length()-1);
+                }
+                //5단계
+                if(answer.isEmpty())
+                    answer="a";
+                //6단계
+                if(answer.length()>=16){
+                    answer=answer.substring(0,15);
+                } 
+                while(answer.charAt(answer.length()-1)=='.'){
+                    answer=answer.substring(0, answer.length()-1);
+                }
+                //7단계
+                while(answer.length()<3){
+                    answer+=answer.charAt(answer.length()-1);
+                }
+                
+                return answer;
+            }
         }
-
-        //마침표 두개 하나로
-        while(new_id.contains("..")){
-            new_id=new_id.replaceAll("..", ".");
-        }
-
-        //마침표가 처음이나 끝에 위치하는지 확인
-        while(new_id.charAt(0)=='.' || new_id.length()=='.'){
-            if(new_id.charAt(0)=='.')
-                new_id=new_id.substring(1, new_id.length());
-            else
-                new_id=new_id.substring(0, new_id.length()-1);
-        }
-
-        //빈 문자열인지 헉인
-        if(new_id.isEmpty()){
-            new_id="a";
-        }
-
-        //길이가 16자 이상인지 확인하고, 넘는다면 15개를 제외한 나머지 모두 제거
-        if(new_id.length()>=16){
-            new_id=new_id.substring(0,15);
-        }
-
-        //마침표가 끝에 있다면 끝에 마침표도 제거    
-        while(new_id.length()=='.'){
-            new_id=new_id.substring(0, new_id.length()-1);
-        }
-
-        //길이가 2자 이하면, 마지막 문자를 길이가 3이 될 때까지 붙임
-        while(new_id.length()<=2){
-            char a=new_id.charAt(new_id.length()-1);
-            new_id=new_id+a;
-        }
-
-        sc.close();
-    }    
+    }
 }
-//47줄?보다 적게 만들어 올 거임 니는 뒤졌다~
