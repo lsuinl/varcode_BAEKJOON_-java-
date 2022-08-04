a4cb081241120f4c11c509cd85e6b16233bff6dc
lsuinl <su96in43@naver.com>
2022-08-05 05:23:14 +0900

    시간초과 죽을사

--- 10week/b10816.java
+++ 10week/b10816.java
@@ -1,38 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class b10816 {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        // 가지고 있는 카드
-        int n = sc.nextInt();
-        System.out.println("야");
-        // 카드입력받기
-        String input = sc.nextLine();
-        System.out.println("야");
-        String[] suin=input.split(" ",n);
-        System.out.println("야");
-        int m = sc.nextInt();
-        System.out.println("야");
-        input = sc.nextLine();
-        System.out.println("야");
-        String[] compare = input.split(" ", m);
-        System.out.println("야");
-        int[] result;
-        System.out.println("야");
-        result= new int[m]; //결과 
-        System.out.println("야");
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int m =Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        String[] suin = input.split(" ");
 
-        //compare의 i번째와 모든 suin의 값을 비교하며 같으면 
-        //1번째 result에 1증가
-        for(int i=0;i<compare.length;i++){
-            for(String e: suin){
-                if(compare[i] == e)
-                    result[i]++;
-            }
-        }
-        System.out.println(result);
-        sc.close();
+        m = Integer.parseInt(br.readLine());
+        input = br.readLine();
+        String[] compare = input.split(" ");
+        
+        for (int i = 0; i < m; i++)
+            bw.write(Collections.frequency(Arrays.asList(suin), compare[i]) + " ");
+        bw.flush();
+        bw.close();
     }
-
 }
